9414f4ea43398e560a7c022d6fd2324f3917c144
Ivan <ipython10@gmail.com>
2019-08-17 14:06:02 +0300

    bugfix

--- sendall/tasks.py
+++ sendall/tasks.py
@@ -21,6 +21,7 @@ def send_message(session, contacts, message, markdown, delay=5):
 
 @shared_task
 def get_dialogs(attrs):
+    print(attrs)
     session, uuid = attrs
     task = ScheduledDialogsTask.objects.get(uuid=uuid)
     dialogs = get_dialogs(session)
