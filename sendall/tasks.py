f1a532eabfc88db9769e96e08ee04555e8f84ef0
Ivan <ipython10@gmail.com>
2019-08-17 14:18:23 +0300

    bugfix

--- sendall/tasks.py
+++ sendall/tasks.py
@@ -24,6 +24,7 @@ def get_dialogs(attrs):
     print(attrs)
     session = attrs[0]
     uuid = attrs[1]
+    print(uuid)
     task = ScheduledDialogsTask.objects.get(uuid=uuid)
     dialogs = get_dialogs(session)
     task.result = json.dumps(dialogs)
