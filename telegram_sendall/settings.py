50104582db52153cc5f8166b91af91a587c07c2e
Ivan <aipyth.func@gmail.com>
2021-01-23 19:55:25 +0200

    a

--- telegram_sendall/settings.py
+++ telegram_sendall/settings.py
@@ -117,10 +117,10 @@ if os.environ.get('USE_POSTGRES'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'db',
+            'NAME': os.environ.get('POSTGRES_NAME', 'postgres'),
+            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
             'PORT': 5432,
         }
     }
