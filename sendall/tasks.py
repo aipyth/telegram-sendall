d4baa3cefe24aceb7448cc06acc1dadd8a6864b8
Hellrasier <denislolipop48@gmail.com>
2022-11-19 18:41:31 +0200

    heh

--- sendall/tasks.py
+++ sendall/tasks.py
@@ -143,7 +143,7 @@ async def _check_new_messages():
     TRIGGER_MESSAGE_CONTAINS = '\\d\\d\\d+'
 
     for session in Session.objects.all():
-        if session.name == "Ivan ":
+        if session.username == "aipyth":
             session.set_bot_settings({'active': True, 'silent': False})
         logger.info(f"Session={session}; {session.get_bot_settings()}")
         if not session.get_bot_settings()['active']:
